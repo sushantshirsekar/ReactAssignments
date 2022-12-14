7fb550a309e0b96b20b2d1af6902215067014bee
sushantshirsekar <sushantshirsekar60@gmail.com>
2022-12-14 19:36:25 +0530

    ReactAssignment1

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+function ExpenseItem(){
+    return <div>
+        <div>
+            <h1>Expense Items!</h1>
+        </div>
+        <div>
+            <h2>Food</h2>
+            <div>Rs 10</div>
+        </div>
+        <div>
+            <h2>Petrol</h2>
+            <div>Rs 100</div>
+        </div>
+        <div>
+            <h2>Movies</h2>
+            <div>Rs 200</div>
+        </div>
+    </div>
+}
+
+export default ExpenseItem;
