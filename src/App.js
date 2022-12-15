97dfdd60ce01de27bdf935ade717c6e0742ff79d
sushantshirsekar <sushantshirsekar60@gmail.com>
2022-12-15 12:42:58 +0530

    reactAssignment Looping

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  let data = [
+    {
+      id: 1,
+      title: "Car Insurance",
+      location: "PhonePe",
+      date: new Date(2021, 11, 31),
+      amount:100
+    },
+    {
+      id: 2,
+      title: "Movie",
+      location: "PVR",
+      date: new Date(2021, 8, 31),
+      amount:500
+    },
+    {
+      id: 3,
+      title: "Dinner",
+      location: "Barbeque",
+      date: new Date(2021, 0, 11),
+      amount:1000
+    },
+    {
+      id: 4,
+      title: "Lunch",
+      location: "The Great Punjab",
+      date: new Date(2021, 3, 29),
+      amount:700
+    },
+  ];
   return (
     <div>
       <h2>Expense Items!</h2>
-      <ExpenseItem></ExpenseItem>
+      {data.map((ele) => {
+        return (
+          <ExpenseItem
+            key={ele.id}
+            title={ele.title}
+            location={ele.location}
+            date={ele.date}
+            amount={ele.amount}
+          ></ExpenseItem>
+        );
+      })}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021 ,11, 31);
-    const expenseAmount = 100;
-    const expenseDescription = 'Petrol';
-    const expenseLocation = 'Mumbai';
+function ExpenseItem(props) {
   return (
     <div>
       <div className="expense-item">
-        <div className="expense-itme__date">{expenseDate.toISOString()}</div>
-        <h2 className="expense-item__description">{expenseLocation}</h2>
-        <h2 className="expense-item__description">{expenseDescription}</h2>
-        <div className="expense-item__price">Rs {expenseAmount}</div>
+        <div className="expense-itme__date">{props.date.toISOString()}</div>
+        <h2 className="expense-item__description">{props.location}</h2>
+        <h2 className="expense-item__description">{props.title}</h2>
+        <div className="expense-item__price">Rs {props.amount}</div>
       </div>
     </div>
   );
