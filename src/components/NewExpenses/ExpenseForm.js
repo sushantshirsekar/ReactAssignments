ec7faea8e70c80363c690ae0ae4d9ca840abe90a
sushantshirsekar <sushantshirsekar60@gmail.com>
2022-12-21 21:17:46 +0530

    SubmitHandler

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,60 +1,70 @@
 import React, { useState } from "react";
 
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEneteredTitle] = useState('');
-    const [enteredAmount, setEneteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
+  const titleHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
 
+  const amountHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
 
-    const titleHandler = (event) => {
-        setEneteredTitle(event.target.value);
-    }
+  const dateHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
 
-    const amountHandler = (event) => {
-        setEneteredAmount(event.target.value);
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-    const dateHandler = (event) => {
-        setEnteredDate(event.target.value);
-    }
-    const submitClick = (e) => {
-        e.preventDefault();
-        const titleInput = e.target.title.value;
-        const amountInput = e.target.amnt.value;
-        const dateInput = e.target.date.value;
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title:</label>
+          <input
+            type="text"
+            onChange={titleHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount:</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountHandler}
 
-        const dataObj = {
-            titleInput,
-            amountInput,
-            dateInput
-        }
-        console.log(dataObj);
-    }
-  return(
-    <form onSubmit={submitClick}>
-    <div className="new-expense__controls">
-      <div className="new-expense__control">
-        <label>Title:</label>
-        <input type="text" name="title" onChange={titleHandler}/>
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date:</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateHandler}
+          />
+        </div>
       </div>
-      <div className="new-expense__control">
-        <label>Amount:</label>
-        <input type="number" min="0.01" step="0.01" name="amnt" onChange={amountHandler} />
-      </div>
-      <div className="new-expense__control">
-        <label>Date:</label>
-        <input type="date" min="2019-01-01" max="2022-12-31" name="date" onChange={dateHandler} />
-      </div>
-    </div>
-    <div className="new-expense__actions">
+      <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
-    </div>
-  </form>
+      </div>
+    </form>
   );
 };
-
 
 export default ExpenseForm;
